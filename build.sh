0d6a710a4c8bd4a69bc2ce10b3d9de0414710ad0
Philippe <hello@philippec.me>
2020-09-14 02:40:11 -0400

    Fix for build script

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker build -t philippec-nginx:1.0 . && docker run -it --rm -d -p 8080:80 --name nginx -v ~/web-data:/usr/share/nginx philippec-nginx:1.0
+docker build -t philippec-nginx:1.0 . && docker run -it --rm -d -p 80:80 -p 443:443 --name nginx -v ~/web-data:/usr/share/nginx philippec-nginx:1.0
