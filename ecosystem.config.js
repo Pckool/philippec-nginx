ec8e20c4883b722d58522b9222a0f8299e401acc
Philippe <hello@philippec.me>
2020-09-14 01:26:20 -0400

    Added a pre-setup script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,6 +12,7 @@ module.exports = {
       repo : 'git@github.com:Pckool/philippec-nginx.git',
       path : '/var/docker/images/nginx',
       'pre-deploy-local': '',
+      'pre-setup': 'npm i -g pm2',
       'post-deploy' : 'pm2 reload ecosystem.config.js --env production',
       'pre-deploy': 'mkdir -p /var/docker || mkdir -p /var/docker/images || mkdir -p /var/docker/images/nginx'
     }
