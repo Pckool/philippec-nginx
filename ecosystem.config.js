6746bb4c4200eaca85776a97bf4c86e080dc334b
Philippe <hello@philippec.me>
2020-09-14 00:45:27 -0400

    check

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,9 +11,9 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:Pckool/philippec-nginx.git',
       path : '/var/docker/images/nginx',
-      'pre-deploy-local': 'mkdir /var/docker || mkdir /var/docker/images || mkdir /var/docker/images/nginx',
+      'pre-deploy-local': '',
       'post-deploy' : 'pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+      'pre-setup': 'mkdir /var/docker || mkdir /var/docker/images || mkdir /var/docker/images/nginx'
     }
   }
 };
