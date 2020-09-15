a95eb7ff2fcd6bfef4a2ec92277f26b5d84bb022
Philippe <hello@philippec.me>
2020-09-14 21:33:00 -0400

    Another change to build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 docker build -t philippec-nginx:1.0 .
-docker run -it -d -p 80:80 -p 443:443 --name nginx -v /usr/share/nginx:/usr/share/nginx -v /ssl:/etc/ssl/certs/docker/nginx/ philippec-nginx:1.0 
+docker run -it -d -p 80:80 -p 443:443 --name nginx -v /usr/share/nginx:/usr/share/nginx -v /etc/ssl/certs/:/etc/ssl/certs/docker/nginx/ philippec-nginx:1.0 
