552102bc96371aaf49a355018da05382780a16a2
Philippe <hello@philippec.me>
2020-09-14 20:54:43 -0400

    This

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 docker build -t philippec-nginx:1.0 .
-docker run -it --rm -d -p 80:80 -p 443:443 --name nginx -v /usr/share/nginx:/usr/share/nginx -v /ssl:/etc/ssl/certs/docker/nginx/ philippec-nginx:1.0 
+docker run -it -d -p 80:80 -p 443:443 --name nginx -v /usr/share/nginx:/usr/share/nginx -v ssl:/etc/ssl/certs/docker/nginx/ philippec-nginx:1.0 
